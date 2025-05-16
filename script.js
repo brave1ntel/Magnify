29a613a6c2a011cbeb0afc9b2fe298cba8ce63e5
Hannah Abcede <abcedehannah@gmail.com>
2025-05-15 23:57:40 -0400

    Added user interaction

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+// ----------------------------Tab Functionality-------------------------------
+
 function showSection(sectionId, element) {
     var sections = document.querySelectorAll('.section');
     sections.forEach(function(section) {
@@ -20,3 +22,27 @@ function showSection(sectionId, element) {
 window.onload = function() {
     showSection('home', document.querySelector('nav a'));
 };
+
+/* --------------------------------------------------------------------- */
+// ----------------------------Tab Interactivity-------------------------------
+
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll(".tab");
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", () => {
+            tabs.forEach(t => t.classList.remove("active"));
+            tab.classList.add("active");
+            tab.scrollIntoView({
+                behavior: "smooth",
+                inline: "center",
+                block: "nearest"
+            })
+        })
+    })
+    const defaultTab = document.getElementById("defaultOpen");
+    if (defaultTab) {
+        defaultTab.click();
+    }
+
+})
